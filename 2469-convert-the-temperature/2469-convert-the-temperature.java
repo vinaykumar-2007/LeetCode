410f9cc2423cad29495b4afb77050d69cfdc6c05
ELUSURI VINAYKUMAR <vinayelusuri175@gmail.com>
2026-06-23 11:58:07 +0530

    Time: 0 ms (100%), Space: 45.9 MB (87.11%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.java
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -1,9 +1,6 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
-        double[] arr = new double[2];
-        arr[0] = celsius + 273.15;
-        arr[1] = celsius * 1.80 + 32.00;
-
+        double[] arr = {celsius + 273.15 , celsius * 1.80 + 32.00};
         return arr;
     }
 }
