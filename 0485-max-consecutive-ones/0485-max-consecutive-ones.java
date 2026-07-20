b06787c05b0ce3ca165c04608f95f4a5b5a42d69
ELUSURI VINAYKUMAR <vinayelusuri175@gmail.com>
2026-07-20 10:53:57 +0530

    Time: 3 ms (60.29%), Space: 52.3 MB (84.03%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -4,11 +4,12 @@ class Solution {
         int count =0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==0){
-                max = Math.max(max,count);
+                
                 count = 0;
             }else{
                 count++;
             }
+            max = Math.max(max,count);
         }
         return Math.max(max,count);
     }
