e71b03a0d9b2147f30686dce4bf9cbb834bdbba8
ELUSURI VINAYKUMAR <vinayelusuri175@gmail.com>
2026-08-29 22:38:15 +0530

    Time: 0 ms (100%), Space: 43.1 MB (16.93%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,12 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        HashSet<Character> set = new HashSet<>();
+
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(set.contains(ch)) return ch;
+            set.add(ch);
+        } 
+        return '1';
+    }
+}
